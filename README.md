1e06e50ae32e2ceea2ca1d6afe0f91dd056242f0
ElouanLeGavroche <d.elouan@laposte.net>
2026-01-06 17:42:11 +0100

    :sparkles: belle avancement du 6 janvier 2026 à 17h42

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # D-livraptor
 Ur program krouet evit merañ urzhioù lec'hiennoù enlinenn evel Alizon.
+
+
+
+
+######
+# DOAREOÙ VIT KOMPREN AR SERVIJ
+
+#    1 [SERVER INFO]
+#        1.1 kevreadenn graet
+#        1.2 keloù resevet
+#        1.3 lennidigezh ar c'heloù
+#        1.4 digas keloioù
+#        1.5 finn ar kojeadenn
+
+#    2 [SERVER DIWAL]
+#        1.1 respont ebet
+#        1.2 respont hir
+#        1.3 arouezenn nan komprenet
+
+#    3 [SERVER GUDEN]
+#        1.1 kevreadenn kolet
+#        1.2 guden barzh kod
+
+
+#    4 [IMPLIJER INFO]
+#    5 [IMPLIJER DIWAl]
+#    6 [IMPLIJER GUDEN]
+
--- servij.c
+++ servij.c
@@ -10,6 +10,7 @@
 
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 
 #include <regex.h>
 
@@ -23,23 +24,50 @@
 #define PONG "PONG"
 
 #define DEMAT "DEMAT"
-#define DEMAT_RESPONT "DEMAT implijer\n"
+#define DEMAT_RESPONT "demat implijer\n"
 
-#define FINN_KOJEADENN "exit"
+#define FINN_KOJEADENN "kuit"
 
 typedef char t_message;
 
+void skriv_kemennadenn(int type, int kemennadenn);
+void lazhan_ur_bugel();
+
+int kemennadenn(int cnx);
+
+/* stadoù 
+    = 0 peptra zo mat
+    = -1 fazi
+*/
+int stad_1();
+int stad_2();
+int stad_3();
+int stad_4();
+int stad_5();
+int stad_6();
+int stad_7();
+int stad_8();
+int stad_9();
+
 int main(){
-	printf("[INFO] Krouidigezh an dalc'h.\n");
+    // Init du système de communication
+	skriv_kemennadenn(4, 1);
+
 	int sock;
 	int ret;
 	struct sockaddr_in addr;
 	int size;
 	int cnx;
 	struct sockaddr_in conn_addr;
+    
+    struct sigaction act;
+
+    act.sa_handler = &lazhan_ur_bugel;
+
+    sigaction(SIGCHLD, &act, NULL);
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	printf("[INFO] Krouidigezh ar soket : %d\n", sock);
+	skriv_kemennadenn(4, 2);
 
 	int opt = 1;
 	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))){
@@ -52,11 +80,12 @@ int main(){
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(8080);
 
-	printf("[INFO] kevreadenn graet\n");
-    int valid = false;
-    pid_t pid = -1;
+    size = sizeof(conn_addr);
 
-    while(valid == false){
+
+
+    while(true){
+
         ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
         ret = listen(sock, 1);
 
@@ -64,56 +93,193 @@ int main(){
             return EXIT_FAILURE;
         }
 
+        cnx = accept(sock, (struct sockaddr *)&conn_addr, (socklen_t *)&size);        
         pid_t pid = fork();
-        if (pid == 0){
-            valid = true;
-        }
-    }
 
-    if(pid == 0){
-        size = sizeof(conn_addr);
-        cnx = accept(sock, (struct sockaddr *)&conn_addr, (socklen_t *)&size);
-        
-        
-        char buffer[HIRDER_KEMENNADENN] = {'\0'};
-
-        while(strncmp(buffer, FINN_KOJEADENN, strlen(FINN_KOJEADENN) - 1) != 0){
-            char message[HIRDER_KEMENNADENN];
-            int taille = 0;
-
-            write(cnx, KEMENNADENN_DEGEMER, strlen(KEMENNADENN_DEGEMER));
+        if(pid == 0){
+            // Ma ez eo ur bugel, mont barzh a ac'hwel vit ober war dro ar kom
+            kemennadenn(cnx);
             
-            /*lecture*/
-            read(cnx, &buffer, HIRDER_KEMENNADENN);			
-            taille = strlen(buffer);
-
-            printf("[INFO] lennidigezh ar keloù\n");
-            ret = printf("[KEMENNADER AN IMPLIJER] %*.*s", taille, taille, buffer);
-
-            if(ret == -1){
-                return EXIT_FAILURE;
-            }
-
-            printf("[INFO] digas keloioù\n");
-            write(cnx, RESPONT_KELOU_RESEVET, sizeof(RESPONT_KELOU_RESEVET));
-
-            if(strncmp(buffer, PING, sizeof(PING) - 1) == 0){
-                int i;
-                for(i = 0; i < 4; i ++){
-                    sprintf(message, "PONG N°%d\n", i);
-                    write(cnx, message, strlen(message));
-
-                }
-            }
-
-            if(strncmp(buffer, DEMAT, sizeof(DEMAT) - 1) == 0){
-                write(cnx, DEMAT_RESPONT, strlen(DEMAT_RESPONT));
-            }
-            
+            shutdown(cnx, SHUT_RDWR);
+            close(cnx);
+            _exit(0);
         }
+        
     }
-
-	printf("[INFO] finn ar kojeadenn\n");
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
+int kemennadenn(int cnx){
+    int ret;
+
+    skriv_kemennadenn(1, 1);
+    
+    
+    char buffer[HIRDER_KEMENNADENN] = {'\0'};
+
+    do{
+         
+        char message[HIRDER_KEMENNADENN];
+        int taille = 0;
+
+        write(cnx, KEMENNADENN_DEGEMER, strlen(KEMENNADENN_DEGEMER));
+        
+        /*lecture*/
+        read(cnx, &buffer, HIRDER_KEMENNADENN);			
+        taille = strlen(buffer);
+
+        skriv_kemennadenn(1, 3);    
+
+        /*MESSAGE DE L'UTILISATEUR*/
+        skriv_kemennadenn(5, 1);
+        ret = printf("%*.*s", taille, taille, buffer);
+        if(ret == -1){
+            return EXIT_FAILURE;
+        }
+
+        skriv_kemennadenn(1, 4);
+
+        if(strncmp(buffer, PING, sizeof(PING) - 1) == 0){
+            int i;
+            for(i = 0; i < 4; i ++){
+                sprintf(message, "PONG N°%d\n", i);
+                write(cnx, RESPONT_KELOU_RESEVET, sizeof(RESPONT_KELOU_RESEVET));
+                write(cnx, message, strlen(message));
+
+            }
+        }
+
+        if(strncmp(buffer, DEMAT, sizeof(DEMAT) - 1) == 0){
+            write(cnx, RESPONT_KELOU_RESEVET, sizeof(RESPONT_KELOU_RESEVET));
+            write(cnx, DEMAT_RESPONT, strlen(DEMAT_RESPONT));
+        }
+        
+    }while(strncmp(buffer, FINN_KOJEADENN, strlen(FINN_KOJEADENN) - 1) != 0);
+
+    skriv_kemennadenn(1, 5);  
+    return 0;
+
+}
+
+void skriv_kemennadenn(int seurt, int kemennadenn){
+    /*
+        1 [SERVER INFO]
+            1.1 kevreadenn graet
+            1.2 keloù resevet
+            1.3 lennidigezh ar c'heloù
+            1.4 digas keloioù
+            1.5 finn ar kojeadenn
+            1.6 bugel lazhet
+
+        2 [SERVER DIWAL]
+            2.1 respont ebet
+            2.2 respont hir
+            2.3 arouezenn nan komprenet
+
+        3 [SERVER GUDEN]
+            3.1 kevreadenn kolet
+            3.2 guden barzh kod
+        4 [SERVER INIT]
+            4.1 Krouidigezh an dalc'h
+            4.2 Krouidigezh ar soket
+        5 [KEMENNADER AN IMPLIJER]
+            5.1
+    */
+
+    switch(seurt){
+        case 1:
+            printf("[SERVER INFO] ");
+            
+            // TRAOÙ NORMAL AR SERVER
+            switch(kemennadenn){
+                case 1:
+                    printf("kevreadenn graet\n");
+                    break;
+
+                case 2:
+                    printf("keloù resevet\n");
+                    break;
+                
+                case 3:
+                    printf("lennidigezh ar c'heloù\n");
+                    break;
+                
+                case 4:
+                    printf("digas keloioù\n");
+                    break;
+
+                case 5:
+                    printf("finn ar kojeadenn\n");
+                    break;
+
+                case 6:
+                    printf("bugel lazhet\n");
+                    break;
+
+                default:
+                    printf("[DIDERMEN]\n");
+                    break;
+            }
+            break;
+
+        // MA ZO GUDENNOÙ GANT AR SERVER
+        case 2:
+            printf("[SERVER GUDEN] ");
+            switch(kemennadenn){
+                default:
+                    printf("netra vit poent\n");
+                    break;
+            }
+            break;
+        
+        // MA ZO GUDENNOÙ GANT AR C'HOD
+        case 3:
+            printf("[SERVER GUDEN] ");
+            switch(kemennadenn){
+                default:
+                    printf("netra vit poent\n");
+                    break;
+            }
+            break;
+        
+            // MA ZO GUDENNOÙ GANT AR C'HOD
+        case 4:
+            printf("[SERVER INIT] ");
+            switch(kemennadenn){
+                case 1:
+                    printf("Krouidigezh an dalc'h.\n");
+                    break;
+                
+                case 2: 
+                    printf("Krouidigezh ar soket.\n");
+                    break;
+
+                default:
+                    printf("netra vit poent\n");
+                    break;
+            }
+            break;
+        
+        case 5:
+            printf("[KEMENNADER AN IMPLIJER]");
+            break;
+
+
+        // VIT AR C'HELLOIOÙ NAN KOMPRENET
+        default:
+            printf("[DIDERMEN] ");
+            switch(kemennadenn){
+                default:
+                    printf("DIWAL, UR FAZHI ZO !\n");
+                    break;
+            }
+            break;
+        
+    }
+}
+
+
+void lazhan_ur_bugel(){
+    skriv_kemennadenn(1, 6);
+
+}
